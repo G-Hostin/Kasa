cb201e8d9e56500a6a0b3d630afbea62cf4b1833
Gregory Hostin <greghostin@icloud.com>
2025-05-23 16:00:51 +0200

    Add content and responsive SCSS styling to 404 NotFound page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ function Banner({ image, text }) {
   return (
     <div className="banner">
       <img src={image} alt="Bannière" />
-      {text && <div className="black-filter-banner"></div>}{" "}
+      {text && <div className="black-filter-banner"></div>}
       {/*Si il a du texte lorsque le composant est appelé (dans Home oui, dans About non), alors ça crée la div black-filter-banner*/}
       <div className="banner-text">
         <h1>{text}</h1>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,17 @@
+import { Link } from "react-router-dom";
+import "../styles/pages/_notfound.scss";
+
 function NotFound() {
-  return <h1>Page 404</h1>;
+  return (
+    <div className="notfoundpage">
+      <h1 className="title-notfound">404</h1>
+      <p className="text-notfound">
+        Oups! La page que vous demandez n'existe plus
+      </p>
+      <Link to="/" className="link-notfound">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
 }
 export default NotFound;
